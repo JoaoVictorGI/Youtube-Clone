e22f54d6a620059b1b6ccfdd03ff5d885ff85d05
JoaoVictorGI <joaovictor.gizidoro@pm.me>
2024-07-29 12:29:28 -0300

    chore: use .env in docker-compose.yml

--- src/mysql.ts
+++ src/mysql.ts
@@ -4,11 +4,11 @@ import { config } from "dotenv";
 config();
 
 const pool = mysql.createPool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  user: process.env.MARIADB_USER,
+  password: process.env.MARIADB_ROOT_PASSWORD,
+  database: process.env.MARIADB_NAME,
+  host: process.env.MARIADB_HOST,
+  port: Number(process.env.MARIADB_PORT),
 });
 
 export { pool };
