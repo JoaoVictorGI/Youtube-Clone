9f2a1cd210689e3f1c036b03a555c73d9d3e8b5f
JoaoVictorGI <joaovictor.gizidoro@pm.me>
2024-09-09 16:30:42 -0300

    feat: login and sign-up using enter Now able to sign-up and login clicking enter in the password input instead on clicking the button

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,44 +1,44 @@
-import type React from "react";
-import { useContext, useState } from "react";
-import { UserContext } from "../../context/userContext";
-import { Button, Container, Input, Link } from "./styles";
+import type React from "react"
+import { useContext, useState } from "react"
+import { UserContext } from "../../context/userContext"
+import { Button, Container, Input, Link } from "./styles"
 
 function Login() {
-	const { handleLogin } = useContext(UserContext);
+  const { handleLogin } = useContext(UserContext)
 
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-	const handleKeyDown = (event: React.KeyboardEvent) => {
-		if (event.key === "Enter") {
-			handleLogin(email, password);
-		}
-	};
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      handleLogin(email, password)
+    }
+  }
 
-	return (
-		<Container>
-			<h1 style={{ fontSize: "32px" }}>Fazer login</h1>
-			<Input
-				type="email"
-				value={email}
-				placeholder="E-mail"
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-			<Input
-				type="password"
-				value={password}
-				placeholder="Senha"
-				onChange={(e) => setPassword(e.target.value)}
-				onKeyDown={handleKeyDown}
-			/>
+  return (
+    <Container>
+      <h1 style={{ fontSize: "32px" }}>Fazer login</h1>
+      <Input
+        type="email"
+        value={email}
+        placeholder="E-mail"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        type="password"
+        value={password}
+        placeholder="Senha"
+        onChange={(e) => setPassword(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
 
-			<Button onClick={() => handleLogin(email, password)}>Entrar</Button>
-			<p>
-				Não tem uma conta?<Link href="sign-up"> Clique aqui</Link> para criar
-				uma conta
-			</p>
-		</Container>
-	);
+      <Button onClick={() => handleLogin(email, password)}>Entrar</Button>
+      <p>
+        Não tem uma conta?<Link href="sign-up"> Clique aqui</Link> para criar
+        uma conta
+      </p>
+    </Container>
+  )
 }
 
-export default Login;
+export default Login
--- client/src/pages/sign-up/index.tsx
+++ client/src/pages/sign-up/index.tsx
@@ -1,42 +1,50 @@
-import { useContext, useState } from "react";
-import { UserContext } from "../../context/userContext";
-import { Button, Container, Input } from "./styles";
+import type React from "react"
+import { useContext, useState } from "react"
+import { UserContext } from "../../context/userContext"
+import { Button, Container, Input } from "./styles"
 
 function SignUp() {
-	const { handleSignUp } = useContext(UserContext);
+  const { handleSignUp } = useContext(UserContext)
 
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-	return (
-		<Container>
-			<h1 style={{ fontSize: "32px" }}>Fazer cadastro</h1>
-			<Input
-				type="name"
-				value={name}
-				placeholder="Nome"
-				onChange={(e) => setName(e.target.value)}
-			/>
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      handleSignUp(name, email, password)
+    }
+  }
 
-			<Input
-				type="email"
-				value={email}
-				placeholder="E-mail"
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-			<Input
-				type="password"
-				value={password}
-				placeholder="Senha"
-				onChange={(e) => setPassword(e.target.value)}
-			/>
+  return (
+    <Container>
+      <h1 style={{ fontSize: "32px" }}>Fazer cadastro</h1>
+      <Input
+        type="name"
+        value={name}
+        placeholder="Nome"
+        onChange={(e) => setName(e.target.value)}
+      />
 
-			<Button onClick={() => handleSignUp(name, email, password)}>
-				Criar conta
-			</Button>
-		</Container>
-	);
+      <Input
+        type="email"
+        value={email}
+        placeholder="E-mail"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        type="password"
+        value={password}
+        placeholder="Senha"
+        onChange={(e) => setPassword(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+
+      <Button onClick={() => handleSignUp(name, email, password)}>
+        Criar conta
+      </Button>
+    </Container>
+  )
 }
 
-export default SignUp;
+export default SignUp
