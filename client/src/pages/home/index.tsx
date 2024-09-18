cbe42fff70d2630c212a2c1d954a7066ed3a5214
João Victor <joaovictor.gizidoro@gmail.com>
2024-09-18 11:40:24 -0300

    chore: some changes

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -2,37 +2,37 @@ import { useEffect, useState } from "react"
 import Categories from "../../components/categories"
 import VideoComponent from "../../components/videoComponent"
 import { Container } from "./styles"
+import VideoIsLoadingComponent from "../../components/videoIsLoading"
 
 interface IProps {
   openMenu: boolean
 }
 
 interface Video {
-    video_id: string
-    thumbnail: string
-    title: string
-    description: string
-    user_id: string
-    name: string
+  video_id: string
+  thumbnail: string
+  title: string
+  description: string
+  user_id: string
+  name: string
 }
-
 
 function Home({ openMenu }: IProps) {
   const [videos, setVideo] = useState<Video[]>([])
 
-useEffect(() => {
+  useEffect(() => {
     fetch("http://localhost:4000/videos/get-all-videos")
-    .then((response) => response.json())
-    .then((data) => {
-      setVideo(data.videos)
-    })
-    .catch((error) => {
-      console.error("Erro buscando vídeos: ", error)
-    })
+      .then((response) => response.json())
+      .then((data) => {
+        setVideo(data.videos)
+      })
+      .catch((error) => {
+        console.error("Erro buscando vídeos: ", error)
+      })
   }, [])
 
   return (
-    <Container >
+    <Container>
       <>
         <Categories />
         {videos.length > 0 ? (
@@ -40,7 +40,7 @@ useEffect(() => {
             <VideoComponent key={video.video_id} video={video} />
           ))
         ) : (
-          <p>Video não encontrados</p>
+          <VideoIsLoadingComponent videos={8} />
         )}
       </>
     </Container>
